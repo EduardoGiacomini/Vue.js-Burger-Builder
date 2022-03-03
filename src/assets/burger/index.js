fc4948367ac26b14f13ff6f9141419de6137c2cc
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2022-03-02 22:44:05 -0400

    fix: fixing eslint errors

--- src/assets/burger/index.js
+++ src/assets/burger/index.js
@@ -1,12 +1,27 @@
-export bacon from './bacon.svg'
-export bunBottom from './bun-bottom.svg'
-export bunTop from './bun-top.svg'
-export cheese from './cheese.svg'
-export egg from './egg.svg'
-export ketchup from './ketchup.svg'
-export mayo from './mayo.svg'
-export meat from './meat.svg'
-export onions from './onions.svg'
-export pickles from './pickles.svg'
-export salad from './salad.svg'
-export tomato from './tomato.svg'
+import * as bacon from './bacon.svg'
+import * as bunBottom from './bun-bottom.svg'
+import * as bunTop from './bun-top.svg'
+import * as cheese from './cheese.svg'
+import * as egg from './egg.svg'
+import * as ketchup from './ketchup.svg'
+import * as mayo from './mayo.svg'
+import * as meat from './meat.svg'
+import * as onions from './onions.svg'
+import * as pickles from './pickles.svg'
+import * as salad from './salad.svg'
+import * as tomato from './tomato.svg'
+
+export {
+  bacon,
+  bunBottom,
+  bunTop,
+  cheese,
+  egg,
+  ketchup,
+  mayo,
+  meat,
+  onions,
+  pickles,
+  salad,
+  tomato
+}
